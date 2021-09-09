f63af6a48ece94f606c60c6b152f48047d3f21d8
AmirAbu-Almajd <amir.abualmajd99@gmail.com>
2021-09-09 11:23:43 +0200

    first try of NUnit
    Successful testing of addition.

--- calculator.Test/Class1.cs
+++ calculator.Test/Class1.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace calculator.Test
 {
-    public class Class1
+    [TestFixture]
+    public class CalculatorTests
     {
+     [Test]
+        public void Calculator_Addition()
+        {
+            double res = Calculator.add(2, 3);
+            Assert.That(res, Is.EqualTo(2));
+        }
+
+
     }
 }
--- calculator/Class1.cs
+++ calculator/Class1.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace calculator
 {
-    public class Class1
+    public class Calculator
     {
+        public static double add(double x, double y)
+        {
+            return x + y;
+        }
+        public static double multiply(double x, double y)
+        {
+            return x * y;
+        }
+        public static double divide(double x, double y)
+        {
+            return x / y;
+        }
+        public static double subtract(double x, double y)
+        {
+            return x - y;
+        }
     }
 }
